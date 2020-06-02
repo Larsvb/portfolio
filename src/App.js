6a766faaaed19581a761dfeca5d4a38f2a79c28c
lars <larsvboven@hotmail.com>
2020-06-02 22:06:14 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <button className="btn">test scss</button>
+    </div>
+  );
+}
+
+export default App;

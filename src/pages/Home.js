267f2716951db5009bd28934a263bdea4602d78c
lars <larsvboven@hotmail.com>
2020-06-03 12:26:07 +0200

    Basic layout and styling
    added basic layout to the Home page and fixed styling for the layout and de title styling. Added variables for styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className="">
-      <h1>Test</h1>
+    <div className="row">
+      <div className="full-row">
+        <div className="container">
+            <div className="title">
+              <h1>Hello i'm <br />Lars van Boven</h1>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }

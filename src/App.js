90f00535ac45c450c0f2c2a33f05420f50bd710d
lars <larsvboven@hotmail.com>
2020-06-03 11:49:13 +0200

    React router and pages
    Added react router & routes. Added the pages that will be needed for my portfolio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './Main.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Skills from './pages/Skills'
+import MyWork from './pages/MyWork'
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <button className="btn">test scss</button>
-    </div>
+    <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/Skills" component={Skills} />
+          <Route exact path="/mywork" component={MyWork} />
+        </Switch>
+      </Router>
   );
 }
 

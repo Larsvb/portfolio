58bb8db96490fbd80500e1ffdae71356dd43d462
lars <larsvboven@hotmail.com>
2020-06-06 11:49:08 +0200

    Nav bar active
    Changed up the layout of the menu to make menu item active color when you are on that page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 function Nav() {
     return(
@@ -9,14 +9,12 @@ function Nav() {
                     <div className='logo'>
                         <h2>Lars van Boven</h2>
                     </div>
-                    <nav>
-                        <ul>
-                            <Link to='/'><li>Home</li></Link>
-                            <Link to='/about'><li>About</li></Link>
-                            <Link to='/skills'><li>Skills</li></Link>
-                            <Link to='/mywork'><li>MyWork</li></Link>
-                        </ul>
-                    </nav>
+                    <div className="menu">
+                        <NavLink className="menu-item" to='/' activeClassName="selected">Home</NavLink>
+                        <NavLink className="menu-item" to='about' activeClassName="selected">About</NavLink>
+                        <NavLink className="menu-item" to='skills'activeClassName="selected">Skills</NavLink>
+                        <NavLink className="menu-item" to='mywork'activeClassName="selected">MyWork</NavLink>
+                    </div>
                 </div>
             </div>
         </div>
